cfa96a79193eec7463d3431875514834d4b919cd
Your Name <you@example.com>
2020-04-14 10:13:40 -0400

    .

--- main.js
+++ main.js
@@ -2,14 +2,17 @@ const Stack = function() {
   return {
     items: [],
 
-    add: function(num) {
-      return this.items.push(num);
+add: function(num) {
+    return this.items.push(num);
     
 },
 remove : function(){
   return this.items.pop();
 
-}
+},
+peek:function(){
+  return this.items[this.items.length-1]
+},
 }
 }
 const Queue = function() {
